501a97deb8b12fd28b7bed1aceb2b0f7de8e6976
Furkan Cingöz <efcingoz@outlook.com>
2023-11-04 11:45:25 +0300

    Initial Commit

---
+++ ShortSwiftUI/ShortSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortSwiftUIApp.swift
+//  ShortSwiftUI
+//
+//  Created by Furkan Cingöz on 4.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShortSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
